f001fbd7b6d729faa96a83c6bcb7b5e5f93edd83
paikar2022 <n1kapa00@students.oamk.fi>
2022-04-27 17:02:41 +0300

    added functions and tested some bootstrap items

--- ss.js
+++ ss.js
@@ -2,67 +2,56 @@
 const OIKEA1 = "Oikea vastaus, hyvä! Kettu on plaplaa jne tähän tekstiä "
 const OIKEA2 = "Oikea vastaus, hyvä! Karhu on plaplaa jne tähän tekstiä "
 const OIKEA3 = "Oikea vastaus, hyvä! Orava on plaplaa jne tähän tekstiä "
-const OIKEA4 = "Oikea vastaus, hyvä! Jänis on plaplaa jne tähän tekstiä "
-const OIKEA5 = "Oikea vastaus, hyvä! Ilves on plaplaa jne tähän tekstiä "
+const OIKEA4 = "Oikea vastaus, hyvä! Ilves on plaplaa jne tähän tekstiä "
+const OIKEA5 = "Oikea vastaus, hyvä! Jänis on plaplaa jne tähän tekstiä "
 const VÄÄRÄ = "Väärä vastaus, harmi! Kokeile uudelleen."
 const PUUTTUU = "Vastaus puuttuu, kirjoita vastaus kuvan alla olevaan kenttään."
 
-// vastaukset ja oikea/väärä
-let answer1 = document.getElementById("right1");
-let answer2 = document.getElementById("right2");
-let answer3 = document.getElementById("right3");
-let answer4 = document.getElementById("right4");
-let answer5 = document.getElementById("right5");
-let wrong = document.getElementById("wrong");
 
+// vastaukset ja oikea/väärä
+let right1 = document.getElementById("right1");
+let right2 = document.getElementById("right2");
+let right3 = document.getElementById("right3");
+let right4 = document.getElementById("right4");
+let right5 = document.getElementById("right5");
+let wrong = document.getElementsByClassName("wrong");
+
+let feedback1 = document.getElementById("feedback1");
+let feedback2 = document.getElementById("feedback2");
+let feedback3 = document.getElementById("feedback3");
+let feedback4 = document.getElementById("feedback4");
+let feedback5 = document.getElementById("feedback5");
 
 
 function v1(){
-    //tähän siit äclassification tms tehtävästä et jos valitsee väärän niin... ja jos oikeen niin...
-    let answer1 = document.getElementById("answer1").value;
-    let result1 = document.getElementById("result1");
-    
-    if (answer1 == "kettu"){result1.textContent = OIKEA1;}
-    else {result1.textContent = VÄÄRÄ; }
-    if (answer1 == "") { result1.textContent = PUUTTUU;}
-
+    document.getElementById("feedback1").textContent = ""; //tyhjäys
+    if (right1.checked == true){feedback1.textContent = OIKEA1;}
+    else {feedback1.textContent = VÄÄRÄ;}
+     //TÄÄ Nyt toimii jotenkin, kun oieka tai väärä valittu
+    //else {feedback1.textContent = VÄÄRÄ; }
+    // if (right1 == "") { feedback1.textContent = PUUTTUU;}
+    //if (right1.checked == false && wrong == "" ) {feedback1.textContent = PUUTTUU;}KOKEILU JOS SAIS TÄN MUTTEI TOIMI
 }
 
 function v2(){
-    document.getElementById("result2").innerHTML = ""; //vastauksen poisto
-    let answer2 = document.getElementById("answer2").value;
-    let result2 = document.getElementById("result2");
-    
-    if (answer2 == "karhu"){ result2.textContent = OIKEA2;}
-    else {result2.textContent  = VÄÄRÄ; }
-    if (answer2 == "") { result2.textContent = PUUTTUU;}
+    document.getElementById("feedback2").textContent = ""; //tyhjäys
+    if (right2.checked == true){feedback2.textContent = OIKEA2;}
+    else {feedback2.textContent = VÄÄRÄ;}
     }
 
-function v3(){
-    document.getElementById("result3").innerHTML = ""; //vastauksen poisto
-    let answer3 = document.getElementById("answer3").value;
-    let result3 = document.getElementById("result3");
-    
-    if (answer3 == "orava"){result3.textContent = OIKEA3;}
-    else {result3.textContent = VÄÄRÄ; }
-    if (answer3 == "") {result3.textContent = PUUTTUU;}
-    }   
+    function v3(){
+        document.getElementById("feedback3").textContent = ""; //tyhjäys
+        if (right3.checked == true){feedback3.textContent = OIKEA3;}
+        else {feedback3.textContent = VÄÄRÄ;}
+        } 
 function v4(){
-    document.getElementById("result4").innerHTML = ""; //vastauksen poisto
-    let answer4 = document.getElementById("answer4").value;
-    let result4 = document.getElementById("result4");
-    
-    if (answer4 == "jänis"){result4.textContent = OIKEA4;}
-    else {result4.textContent = VÄÄRÄ; }
-    if (answer4 == "") {result4.textContent = PUUTTUU;}
+    document.getElementById("feedback4").textContent = ""; //tyhjäys
+    if (right4.checked == true){feedback4.textContent = OIKEA4;}
+    else {feedback4.textContent = VÄÄRÄ;}
     }
 
 function v5(){
-    document.getElementById("result5").innerHTML = ""; //vastauksen poisto
-    let answer5 = document.getElementById("answer5").value;
-    let result5 = document.getElementById("result5");
-    
-    if (answer5 == "ilves"){result5.textContent = OIKEA5;}
-    else {result5.textContent = VÄÄRÄ; }
-    if (answer5 == "") {result5.textContent = PUUTTUU;}
+    document.getElementById("feedback5").textContent = ""; //tyhjäys
+    if (right5.checked == true){feedback5.textContent = OIKEA5;}
+    else {feedback5.textContent = VÄÄRÄ;}
     }
