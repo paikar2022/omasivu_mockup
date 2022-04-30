6db5c19293698a7d38e52e2dcef4c0a7d4d1e316
paikar2022 <n1kapa00@students.oamk.fi>
2022-04-30 12:38:50 +0300

    started array testing

--- ss.js
+++ ss.js
@@ -22,7 +22,8 @@ let feedback3 = document.getElementById("feedback3");
 let feedback4 = document.getElementById("feedback4");
 let feedback5 = document.getElementById("feedback5");
 
-function start() {document.getElementById("kettu").style.display = "block"};
+function start() {
+    document.getElementById("kettu").style.display = "block"};
 
 function v1(){
     document.getElementById("feedback1").textContent = ""; //tyhjäys
@@ -74,7 +75,31 @@ function v5(){
 
     document.getElementById("seuraava1").onclick = function(){
     document.getElementById("jänis").style.display ="none";
-     document.getElementById("kettu").style.display ="block";
+    document.getElementById("kettu").style.display ="block";
         }
     //tai tekisikö tähän vaihtoehdon että uudestaan tai lopetus ja siihen sitten lopputilanne? pitää muutes rakentaa että tyhjentää vielä kaikki jos alkaa alusta.
 }
+
+//testing array
+let fox = document.getElementById("fox");
+let bear = document.getElementById("bear");
+let squirrel= document.getElementById("squirrel");
+let lynx = document.getElementById("lynx");
+let hare = document.getElementById("hare");
+
+
+function randomQuestion(min, max){
+    return Math.floor(Math.random() * (max - min +1) ) + min;
+}
+
+let questionsMin = Number(1);
+let questionsMax = Number(questionsArray.length);
+let newArray = new Array();
+
+
+for (let questionsArray = [fox, bear, squirrel, lynx, hare], i = questionsArray.length; i--; ) {
+    let random = questionsArray.splice(Math.floor(Math.random() * i),1)[0];
+    document.getElementById("random").innerHTML += random++ + " ";
+}
+
+
